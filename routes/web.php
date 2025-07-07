2455e57e91b80ca8417c236f95dcdc8990332295
brngbn <brngbncoding@gmail.com>
2025-07-07 18:58:16 +0800

    feat: update routes

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,23 @@
 use App\Http\Controllers\Admin\OrderController as CustomerOrderController;
 use App\Http\Controllers\Admin\ProductController as PublicProductController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 use App\Http\Controllers\HomeController;
 
 // Public customer-facing routes
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+    ]);
+})->name('welcome');
+
 Route::get('/products/{product}', [PublicProductController::class, 'show'])->name('products.show');
 
 // Authenticated user routes
 Route::middleware(['auth', 'verified'])->group(function() {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
     // Customer viewing their own orders
     Route::get('/my-orders', [CustomerOrderController::class, 'index'])->name('my-orders.index');
     Route::get('/my-orders/{order}', [CustomerOrderController::class, 'show'])->name('my-orders.show');
