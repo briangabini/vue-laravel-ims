2db3eca0567274c5288f6847440411006c987377
brngbn <brngbncoding@gmail.com>
2025-07-20 16:54:17 +0800

    feat: create a root layout

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,4 +1,5 @@
 import '../css/app.css';
+import 'vue-sonner/style.css';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
@@ -6,6 +7,7 @@ import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
 import { initializeTheme } from './composables/useAppearance';
+import RootLayout from './layouts/RootLayout.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,7 +15,9 @@ createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+        createApp({
+            render: () => h(RootLayout, null, { default: () => h(App, props) }),
+        })
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
