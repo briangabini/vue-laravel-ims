d59bc39f9d4d7f7ec005767d41c29fd209b46b07
brngbn <brngbncoding@gmail.com>
2025-07-20 16:54:08 +0800

    feat: add sonner from shadcn-vue

--- resources/js/components/AppContent.vue
+++ resources/js/components/AppContent.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { SidebarInset } from '@/components/ui/sidebar';
-import { computed } from 'vue';
+import { toast } from 'vue-sonner';
+import { usePage } from '@inertiajs/vue3';
+import { computed, onMounted } from 'vue';
 
 interface Props {
     variant?: 'header' | 'sidebar';
@@ -9,6 +11,26 @@ interface Props {
 
 const props = defineProps<Props>();
 const className = computed(() => props.class);
+
+const page = usePage();
+
+onMounted(() => {
+    /*if (page.props.flash?.last_login_attempt) {
+        const { successful, ip_address, logged_in_at } = page.props.flash.last_login_attempt;
+        const title = successful ? 'Successful Login' : 'Failed Login Attempt';
+        const description = successful
+            ? `The last successful login was from IP address ${ip_address} on ${logged_in_at}.`
+            : `The last failed login attempt was from IP address ${ip_address} on ${logged_in_at}.`;
+
+        toast(title, {
+            description: description,
+        });
+    }*/
+    toast('Welcome back!', {
+        description: 'You have successfully logged in.',
+        duration: 5000,
+    });
+});
 </script>
 
 <template>
@@ -19,3 +41,4 @@ const className = computed(() => props.class);
         <slot />
     </main>
 </template>
+
